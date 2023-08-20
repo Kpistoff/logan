fbb689224ef1df9ed38a8ff9c05ac5c98c4e4975
Kpistoff <116204658+Kpistoff@users.noreply.github.com>
2023-08-20 13:55:56 +0300

    server.js

--- login_form/server.js
+++ login_form/server.js
@@ -2,9 +2,9 @@ if(process.env.NODE_ENV !== "production"){
   require("dotenv").config()
 }
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 80; // Порт 80
 const bcrypt = require('bcrypt');
 const initializePassport = require("./passport-config");
 const passport = require('passport');
@@ -96,6 +96,6 @@ function checkNotAuthenticated(req, res, next){
 
 
 // Запуск сервера
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
 });
